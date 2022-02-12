48e65e56804160dde4731e552414e5a5ecd5e66b
zinhoCerqueira <45018490+zinhoCerqueira@users.noreply.github.com>
2022-02-12 20:36:33 -0300

    Mais algumas coisas na home, a descrição e a apresentação e um pequeno avanço também na navbar.

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,40 +1,30 @@
 import Link from 'next/Link'
 import styles from '../styles/Navbar.module.css'
-import Button from './Button'
 
 export default function Navbar() {
   return (
-    <ul className={styles.navbar}>
+    <div className={styles.navbar}>
       <div className={styles.lista}>
-        <li >
-          <Link href='/'>
-            <a>Promoções</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/produtos'>
-            <a>Meu Carrinho</a>
-          </Link>
-        </li>
-        <li>
-          <Link href='/sobre'>
-            <a>Serviços</a>
-          </Link>
-        </li>
-      </div>
 
-      <div>
-        <ul className={styles.listaButtons}>
-          <li>
-            <Button />
-          </li>
-          <li>
-            <Button />
-          </li>
-        </ul>
+        <Link href='/'>
+          <a>Promoções</a>
+        </Link>
+        <Link href='/produtos'>
+          <a>Meu Carrinho</a>
+        </Link>
+        <Link href='/sobre'>
+          <a>Serviços</a>
+        </Link>
 
       </div>
 
-    </ul>
+      <div className={styles.listaButtons}>
+
+        <button type="button">Login</button>
+        <button className = {styles.ajuste} type="button">Cadastre-se</button>
+
+      </div>
+
+    </div>
   )
 }
--- pages/index.js
+++ pages/index.js
@@ -29,6 +29,32 @@ export default function Home() {
             height="285px"
           />
         </div>
+      </div>
+
+      <div className={styles.present3}>
+        <div className={styles.apresentation}>
+
+          <div className={styles.card}>
+            eqweqeqeqwqw
+          </div>
+
+          <div className={styles.card}>
+            wqeqeeqeq
+          </div>
+
+          <div className={styles.card}>
+            eqwe
+          </div>
+
+        </div>
+
+        <div className={styles.description}>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <br></br>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
 
       </div>
 
