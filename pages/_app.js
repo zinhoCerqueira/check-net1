476a5ea6f6ddb29d4e37f98045167e4f7a36e2f1
zinhoCerqueira <45018490+zinhoCerqueira@users.noreply.github.com>
2022-02-28 21:36:29 -0300

    Algumas coisas do form.

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,5 @@
 import MainContainer from '../components/MainContainer'
 import '../styles/globals.css'
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDzH-6cfx41Q3tkwYDUGnoxB5_7Akeoq7Y",
-  authDomain: "rangocheck.firebaseapp.com",
-  projectId: "rangocheck",
-  storageBucket: "rangocheck.appspot.com",
-  messagingSenderId: "879140043359",
-  appId: "1:879140043359:web:47217d4695bc805c8439ea",
-  measurementId: "G-MZMMH5NYDP"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/cadastro.js
+++ pages/cadastro.js
@@ -4,6 +4,33 @@ import Form from 'react-bootstrap/Form'
 
 export default function Cadastro() {
 
+  function valueForm() {
+    
+    const form = {
+      nome : String,
+      email : String,
+      email2 : String,
+      senha : String,
+      senha2 : String,
+      checkBox :String,
+    }
+
+    form.nome = document.getElementById('nome').value;
+    form.email = document.getElementById('email').value;
+    form.email2 = document.getElementById('email2').value;
+    form.senha = document.getElementById('senha').value;
+    form.senha2 = document.getElementById('senha2').value;
+    form.checkBox = document.getElementById('checkbox').value;
+
+    console.log(document.getElementById('checkbox').value);
+    if (document.getElementById('checkbox').value.checked){
+      console.log("Checkado")
+    }
+    else{
+      console.log("Não checkado")
+    }
+  };
+
   return (
     <>
       <Navbar />
@@ -11,30 +38,30 @@ export default function Cadastro() {
         <h2>Crie sua conta no <span >RangoCheck</span></h2>
         <div className={styles.formContainer}>
 
-          <Form>
-            <Form.Group className={styles.form}>
+          <Form >
+            <Form.Group  className={styles.form}>
               
               <Form.Label className={styles.label}>Nome</Form.Label>
-              <Form.Control controlId = 'nome' className={styles.control} type="default" placeholder="Seu nome" />
+              <Form.Control  id = 'nome' className={styles.control} type="default" placeholder="Seu nome" />
               
               <Form.Label className={styles.label}>Email</Form.Label>
-              <Form.Control className={styles.control} type="email" placeholder="Seu e-mail" />
+              <Form.Control id = 'email' className={styles.control} type="email" placeholder="Seu e-mail" />
              
               <Form.Label className={styles.label}>Repita Seu Email</Form.Label>
-              <Form.Control className={styles.control} type="email" placeholder="Seu e-mail" />
+              <Form.Control id = 'email2' className={styles.control} type="email" placeholder="Seu e-mail" />
               
               <Form.Label className={styles.label}>Senha</Form.Label>
-              <Form.Control className={styles.control} type="password" placeholder="Password" />
+              <Form.Control id = 'senha' className={styles.control} type="password" placeholder="Password" />
               
               <Form.Label className={styles.label}>Repita Sua Senha</Form.Label>
-              <Form.Control className={styles.control} type="password" placeholder="Password" />
+              <Form.Control id = 'senha2' className={styles.control} type="password" placeholder="Password" />
               
-              <Form.Check className={styles.checkBox} type="checkbox" label=" Aceito os termos de uso e política de privacidade." />
+              <Form.Check id = 'checkbox' className={styles.checkBox} type="checkbox" label=" Aceito os termos de uso e política de privacidade." />
             
             </Form.Group>
           </Form>
 
-          <button type='submit'>Cadastre-se</button>
+          <button type='submit' onClick={valueForm}>Cadastre-se</button>
 
         </div>
       </div>
